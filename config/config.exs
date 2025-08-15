13cffd28479fab392ecdb2378b65207ae996a7a3
oliphaunte <9771752+Oliphaunte@users.noreply.github.com>
2025-08-15 19:03:45 -0400

    Add authentication and authorization layer with Liveview

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :investor_portal, :scopes,
+  user: [
+    default: true,
+    module: InvestorPortal.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: InvestorPortal.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :investor_portal,
   ecto_repos: [InvestorPortal.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
